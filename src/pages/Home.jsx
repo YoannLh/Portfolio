c7c05c9100409b0cc88c9127e3fae0d3a8070355
L'hostis <yolhostis@live.fr>
2022-05-12 19:26:22 +0200

    Essais design home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
+
+import photo from '../assets/images/background.jpeg'
 
 const StyledContainer = styled.section`
     display: flex;
@@ -7,14 +9,60 @@ const StyledContainer = styled.section`
     height: 700px;
 `
 
-const StyledText = styled.p`
-    margin: auto;
+const StyledTextName = styled.p`
+    position: absolute;
+    top: 45%;
+    left: 3%;
+    font-size: 4em;
+    font-weight: bold;
+`
+
+const StyledTextJob = styled.p`
+    position: absolute;
+    top: 56.5%;
+    left: 22%;
+    font-size: 2em;
+`
+
+const StyledTextFreelance = styled.p`
+    position: absolute;
+    top: 220px;
+    left: 30%;
+    transform: rotate(270deg);
+    font-size: 3em;
+`
+
+const StyledTextRemote = styled.p`
+    position: absolute;
+    top: 455px;
+    left: 15%;
+    transform: rotate(270deg);
+    font-size: 3em;
+`
+
+const StyledFirstSubContainer = styled.section`
+    width: 50%;
+    height: 750px;
+    background: rgba(235, 235, 235, 0.6);
+`
+
+const StyledSecondSubContainer = styled.section`
+    width: 50%;
+    height: 750px;
+    background: url(${photo});
+    object-fit: cover;
 `
 
 export default function Home() {
     return (
         <StyledContainer>
-            <StyledText>Home</StyledText>
+            <StyledFirstSubContainer>
+                <StyledTextName>YOANN L'HOSTIS</StyledTextName>
+                <StyledTextJob>Développeur Front-End</StyledTextJob>
+                <StyledTextFreelance>Freelance</StyledTextFreelance>
+                <StyledTextRemote>Remote</StyledTextRemote>
+            </StyledFirstSubContainer>
+            <StyledSecondSubContainer></StyledSecondSubContainer>
         </StyledContainer>
     )
 }
